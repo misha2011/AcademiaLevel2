71cb0b899fc8d797b09ce32fb46d92bd1d780174
shmyh <shmyh@DESKTOP-5SLUO0F>
2016-12-13 13:46:46 +0200

    Add project files.

---
+++ AcademiaLevel2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcademiaLevel2.Startup))]
+namespace AcademiaLevel2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
